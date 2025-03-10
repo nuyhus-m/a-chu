7381714fa9e5111958f915af2c2579669b0e9d9d
김덕윤 <duk9741@gmail.com>
2025-03-10 14:57:29 +0900

    feat: 기본 예외 핸들링 구현

---
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/response/ApiResponse.java
@@ -0,0 +1,59 @@
+package com.ssafy.s12p21d206.achu.api.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ssafy.s12p21d206.achu.api.error.CoreApiErrorType;
+import com.ssafy.s12p21d206.achu.api.error.ErrorMessage;
+import com.ssafy.s12p21d206.achu.domain.error.CoreErrorType;
+
+public class ApiResponse<S> {
+
+  private final ResultType result;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private final S data;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private final ErrorMessage error;
+
+  private ApiResponse(ResultType result, S data, ErrorMessage error) {
+    this.result = result;
+    this.data = data;
+    this.error = error;
+  }
+
+  public static ApiResponse<?> success() {
+    return new ApiResponse<>(ResultType.SUCCESS, null, null);
+  }
+
+  public static <S> ApiResponse<S> success(S data) {
+    return new ApiResponse<>(ResultType.SUCCESS, data, null);
+  }
+
+  public static ApiResponse<?> error(CoreApiErrorType error) {
+    return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error));
+  }
+
+  public static ApiResponse<?> error(CoreApiErrorType error, Object errorData) {
+    return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error, errorData));
+  }
+
+  public static ApiResponse<?> error(CoreErrorType error) {
+    return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error));
+  }
+
+  public static ApiResponse<?> error(CoreErrorType error, Object errorData) {
+    return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error, errorData));
+  }
+
+  public ResultType getResult() {
+    return result;
+  }
+
+  public Object getData() {
+    return data;
+  }
+
+  public ErrorMessage getError() {
+    return error;
+  }
+}
---
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/response/ResultType.java
@@ -0,0 +1,6 @@
+package com.ssafy.s12p21d206.achu.api.response;
+
+public enum ResultType {
+  SUCCESS,
+  ERROR,
+}
