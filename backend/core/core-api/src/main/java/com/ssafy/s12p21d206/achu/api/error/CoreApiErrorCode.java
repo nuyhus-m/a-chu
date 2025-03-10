3c3ef70f5b4f249135697a90f0927c74444898c9
김덕윤 <duk9741@gmail.com>
2025-03-10 14:57:16 +0900

    feat: API 예외 클래스 추가

---
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/error/CoreApiErrorCode.java
@@ -0,0 +1,5 @@
+package com.ssafy.s12p21d206.achu.api.error;
+
+public enum CoreApiErrorCode {
+  AE1000
+}
---
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/error/ErrorMessage.java
@@ -0,0 +1,50 @@
+package com.ssafy.s12p21d206.achu.api.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ssafy.s12p21d206.achu.domain.error.CoreErrorType;
+
+public class ErrorMessage {
+
+  private final String code;
+
+  private final String message;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private final Object data;
+
+  public ErrorMessage(CoreErrorType errorType) {
+    this.code = errorType.getCode().name();
+    this.message = errorType.getMessage();
+    this.data = null;
+  }
+
+  public ErrorMessage(CoreErrorType errorType, Object data) {
+    this.code = errorType.getCode().name();
+    this.message = errorType.getMessage();
+    this.data = data;
+  }
+
+  public ErrorMessage(CoreApiErrorType errorType) {
+    this.code = errorType.getCode().name();
+    this.message = errorType.getMessage();
+    this.data = null;
+  }
+
+  public ErrorMessage(CoreApiErrorType errorType, Object data) {
+    this.code = errorType.getCode().name();
+    this.message = errorType.getMessage();
+    this.data = data;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public Object getData() {
+    return data;
+  }
+}
