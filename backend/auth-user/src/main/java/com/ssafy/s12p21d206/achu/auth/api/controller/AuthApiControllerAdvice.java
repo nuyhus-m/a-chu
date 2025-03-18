bb2e3ed517f88d64efc03472e091ad17fcddada3
김덕윤 <duk9741@gmail.com>
2025-03-18 17:14:30 +0900

    feat: auth-user 모듈 에러 처리 advice 추가
    Former-commit-id: 1a5429168f3a768b74d987879cca9e18c815f930

---
+++ backend/auth-user/src/main/java/com/ssafy/s12p21d206/achu/auth/api/controller/AuthApiControllerAdvice.java
@@ -0,0 +1,70 @@
+package com.ssafy.s12p21d206.achu.auth.api.controller;
+
+import com.ssafy.s12p21d206.achu.auth.api.error.AuthCoreApiErrorType;
+import com.ssafy.s12p21d206.achu.auth.api.error.AuthCoreApiException;
+import com.ssafy.s12p21d206.achu.auth.api.response.AuthApiResponse;
+import com.ssafy.s12p21d206.achu.auth.domain.error.AuthCoreErrorKind;
+import com.ssafy.s12p21d206.achu.auth.domain.error.AuthCoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses = AuthApiControllerAdvice.class)
+public class AuthApiControllerAdvice {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  private static final String CORE_API_EXCEPTION_LOG_FORMAT = "CoreApiException : {}";
+  private static final String CORE_EXCEPTION_LOG_FORMAT = "CoreException : {}";
+  private static final String EXCEPTION_LOG_FORMAT = "Exception : {}";
+
+  @ExceptionHandler(AuthCoreApiException.class)
+  public ResponseEntity<AuthApiResponse<Void>> handleCoreApiException(AuthCoreApiException e) {
+    switch (e.getErrorType().getLevel()) {
+      case ERROR:
+        log.error(CORE_API_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+      case WARN:
+        log.warn(CORE_API_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+      default:
+        log.info(CORE_API_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+    }
+
+    return new ResponseEntity<>(
+        AuthApiResponse.error(e.getErrorType()), e.getErrorType().getStatus());
+  }
+
+  @ExceptionHandler(AuthCoreException.class)
+  public ResponseEntity<AuthApiResponse<Void>> handleCoreException(AuthCoreException e) {
+    switch (e.getErrorType().getLevel()) {
+      case ERROR:
+        log.error(CORE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+      case WARN:
+        log.warn(CORE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+      default:
+        log.info(CORE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+        break;
+    }
+
+    HttpStatus status = e.getErrorType().getKind() == AuthCoreErrorKind.CLIENT_ERROR
+        ? HttpStatus.BAD_REQUEST
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    return new ResponseEntity<>(AuthApiResponse.error(e.getErrorType()), status);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<AuthApiResponse<Void>> handleException(Exception e) {
+    log.error(EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+    return new ResponseEntity<>(
+        AuthApiResponse.error(AuthCoreApiErrorType.INTERNAL_SERVER_ERROR),
+        AuthCoreApiErrorType.INTERNAL_SERVER_ERROR.getStatus());
+  }
+}
