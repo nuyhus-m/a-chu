d9a4cbe80a50db0558c2c797c31364ee5d1d52c5
nuyhus-m <minsh2344@gmail.com>
2025-03-18 17:37:00 +0900

    Refactor[AOS] - BottomBar 코드 구조 변경

--- android/app/src/main/java/com/ssafy/achu/core/components/BottomNavBar.kt
+++ android/app/src/main/java/com/ssafy/achu/core/components/BottomNavBar.kt
@@ -1,44 +1,63 @@
 package com.ssafy.achu.core.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.ssafy.achu.core.navigation.BottomNavScreen.Companion.HOME
 import com.ssafy.achu.core.navigation.bottomNavBarScreens
+import com.ssafy.achu.core.theme.White
 
 @Composable
 fun BottomNavBar(navController: NavHostController) {
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    // 현재 화면 루트
+    val currentRoute by remember {
+        derivedStateOf {
+            currentBackStack?.destination?.route ?: HOME
+        }
+    }
 
-    NavigationBar (
-        containerColor = Color.White
+    AnimatedVisibility(
+        visible = bottomNavBarScreens.map { it.route }.contains(currentRoute),
+        enter = fadeIn(),
+        exit = fadeOut()
     ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination = navBackStackEntry?.destination
+        NavigationBar(
+            containerColor = White
+        ) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-        bottomNavBarScreens.forEach { screen ->
-            NavigationBarItem(
-                icon = { Icon(painterResource(screen.icon), contentDescription = null) },
-                label = { Text(stringResource(screen.titleResId)) },
-                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+            bottomNavBarScreens.forEach { screen ->
+                NavigationBarItem(
+                    icon = { Icon(painterResource(screen.icon), contentDescription = null) },
+                    label = { Text(stringResource(screen.titleResId)) },
+                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
