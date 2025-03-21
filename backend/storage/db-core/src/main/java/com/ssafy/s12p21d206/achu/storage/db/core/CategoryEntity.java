913a95c874fb7b49e736abe76fa61a6ea2684713
PARK Jaeyoung <estel2005@naver.com>
2025-03-21 14:54:59 +0900

    feat: 카테고리 관련 Storage 구현
    Former-commit-id: 835cbf8e11b2a8d0e1da1cf5a0d73752b6ccd13c

---
+++ backend/storage/db-core/src/main/java/com/ssafy/s12p21d206/achu/storage/db/core/CategoryEntity.java
@@ -0,0 +1,22 @@
+package com.ssafy.s12p21d206.achu.storage.db.core;
+
+import com.ssafy.s12p21d206.achu.domain.Category;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "category")
+@Entity
+public class CategoryEntity extends BaseEntity {
+
+  private String name;
+
+  protected CategoryEntity() {}
+
+  public CategoryEntity(String name) {
+    this.name = name;
+  }
+
+  public Category toCategory() {
+    return new Category(getId(), name);
+  }
+}
