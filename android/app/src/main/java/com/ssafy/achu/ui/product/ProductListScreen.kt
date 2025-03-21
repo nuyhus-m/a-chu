9d819338ba063581e747e20000138c14c3b41f7a
nuyhus-m <minsh2344@gmail.com>
2025-03-21 17:12:31 +0900

    Design[AOS] - ProductListScreen UI 구현

--- android/app/src/main/java/com/ssafy/achu/ui/product/ProductListScreen.kt
+++ android/app/src/main/java/com/ssafy/achu/ui/product/ProductListScreen.kt
@@ -1,25 +1,136 @@
 package com.ssafy.achu.ui.product
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ssafy.achu.R
+import com.ssafy.achu.core.components.PointBlueLineBtn
+import com.ssafy.achu.core.theme.AchuTheme
 
 @Composable
-fun ProductListScreen(modifier: Modifier = Modifier) {
+fun ProductListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ProductListViewModel = ProductListViewModel()
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
 
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = modifier
+            .fillMaxSize()
     ) {
-        Text(
-            text = "중고거래 화면",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column {
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Box(
+                modifier = modifier
+                    .padding(horizontal = 24.dp)
+            ) {
+                SearchBar(
+                    value = uiState.query,
+                    onValueChange = { viewModel.updateQuery(it) }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            val items = listOf("All", "의류", "교구/완구", "잡화", "출산용", "버튼 6", "버튼 7", "버튼 8")
+            HorizontalButtonList(
+                items = items,
+                onButtonClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchBar(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(R.string.search_icon)
+            )
+        },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.searchbar_placeholder),
+                style = AchuTheme.typography.regular16.copy(color = Color(0xFFC4C4C4))
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFFF3F3F3), shape = RoundedCornerShape(16.dp)),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent,
+            cursorColor = Color.Black
+        ),
+    )
+}
+
+@Composable
+fun HorizontalButtonList(
+    items: List<String>,
+    onButtonClick: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    initialSelectedIndex: Int = 0
+) {
+    var selectedIndex by remember { mutableIntStateOf(initialSelectedIndex) }
+
+    LazyRow(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items.withIndex().toList()) { (index, item) ->
+            val isSelected = index == selectedIndex
+
+            PointBlueLineBtn(buttonText = item, isSelected = isSelected) {
+                selectedIndex = index
+                onButtonClick(item)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductListScreenPreview() {
+    AchuTheme {
+        ProductListScreen()
     }
 }
---
+++ android/app/src/main/java/com/ssafy/achu/ui/product/ProductListViewModel.kt
@@ -0,0 +1,17 @@
+package com.ssafy.achu.ui.product
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class ProductListViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ProductListUIState())
+    val uiState: StateFlow<ProductListUIState> = _uiState.asStateFlow()
+
+    fun updateQuery(query: String) {
+        _uiState.update { it.copy(query = query) }
+    }
+}
