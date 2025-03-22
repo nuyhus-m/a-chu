abe4f9ad52d98f99e34d3da03192d07b48fbbadc
김덕윤 <duk9741@gmail.com>
2025-03-22 15:31:46 +0900

    test: VerificationCodeBuilder를 통한 VerificationCode 생성
    Former-commit-id: 0d22a9aa07e4713b66d150838034d5e6ad5f750c

--- backend/auth-user/src/test/java/com/ssafy/s12p21d206/achu/auth/domain/VerificationCodeMessageGeneratorTest.java
+++ backend/auth-user/src/test/java/com/ssafy/s12p21d206/achu/auth/domain/VerificationCodeMessageGeneratorTest.java
@@ -2,10 +2,8 @@ package com.ssafy.s12p21d206.achu.auth.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import com.ssafy.s12p21d206.achu.auth.domain.support.AuthDefaultDateTime;
-import java.time.Duration;
+import com.ssafy.s12p21d206.achu.builder.VerificationCodeBuilder;
 import java.time.LocalDateTime;
-import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,25 +20,15 @@ class VerificationCodeMessageGeneratorTest {
   void 회원_가입_인증_메세지를_생성_할_수_있다() {
     // given
     VerificationPurpose purpose = VerificationPurpose.SIGN_UP;
-    String verificationId = UUID.randomUUID().toString();
-    String code = "123456";
-    Duration expiresIn = Duration.ofMinutes(3);
-    VerificationCode verificationCode = new VerificationCode(
-        UUID.randomUUID(),
-        new Phone("01012341234"),
-        code,
-        purpose,
-        false,
-        expiresIn,
-        new AuthDefaultDateTime(LocalDateTime.now(), LocalDateTime.now()));
-
+    VerificationCode verificationCode =
+        VerificationCodeBuilder.createNonVerified(purpose, LocalDateTime.now());
     // when
     String verificationCodeMessage = sut.generate(verificationCode, purpose);
 
     // then
     assertThat(verificationCodeMessage)
-        .contains(code)
-        .contains(purpose.getDescription())
-        .contains(String.valueOf(expiresIn.getSeconds()));
+        .contains(verificationCode.code())
+        .contains(verificationCode.purpose().getDescription())
+        .contains(String.valueOf(verificationCode.expiresIn().getSeconds()));
   }
 }
