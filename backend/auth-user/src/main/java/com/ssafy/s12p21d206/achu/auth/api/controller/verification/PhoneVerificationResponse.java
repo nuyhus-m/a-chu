5d02b09d63c6100f7c09469e9173b9aaa6f0fe0a
김덕윤 <duk9741@gmail.com>
2025-03-22 15:54:19 +0900

    refactor: 인증번호 생성 응답 id 필드명 변경
    Former-commit-id: 4bd53f5001c92317ea3e6bdeb40731616ad15435

--- backend/auth-user/src/main/java/com/ssafy/s12p21d206/achu/auth/api/controller/verification/PhoneVerificationResponse.java
+++ backend/auth-user/src/main/java/com/ssafy/s12p21d206/achu/auth/api/controller/verification/PhoneVerificationResponse.java
@@ -4,7 +4,7 @@ import com.ssafy.s12p21d206.achu.auth.domain.VerificationCode;
 import java.time.Duration;
 import java.util.UUID;
 
-public record PhoneVerificationResponse(UUID verificationId, Duration expiresIn) {
+public record PhoneVerificationResponse(UUID id, Duration expiresIn) {
 
   public static PhoneVerificationResponse of(VerificationCode phoneVerificationCode) {
     return new PhoneVerificationResponse(
