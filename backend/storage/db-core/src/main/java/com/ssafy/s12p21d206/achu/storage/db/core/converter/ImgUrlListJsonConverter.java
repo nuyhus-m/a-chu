2e37444278c3033563741edc37017002b323fbd5
PARK Jaeyoung <estel2005@naver.com>
2025-03-24 16:58:06 +0900

    feat: 이미지 URL 리스트 저장을 위한 JSON Converter 추가
    Former-commit-id: 0965a324e386665ff6fc84819ad8b706b590bc11

---
+++ backend/storage/db-core/src/main/java/com/ssafy/s12p21d206/achu/storage/db/core/converter/ImgUrlListJsonConverter.java
@@ -0,0 +1,30 @@
+package com.ssafy.s12p21d206.achu.storage.db.core.converter;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
+import java.util.List;
+
+@Convert
+public class ImgUrlListJsonConverter implements AttributeConverter<List<String>, String> {
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public String convertToDatabaseColumn(List<String> attribute) {
+    try {
+      return objectMapper.writeValueAsString(attribute);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Failed to convert list to JSON", e);
+    }
+  }
+
+  @Override
+  public List<String> convertToEntityAttribute(String dbData) {
+    try {
+      return objectMapper.readValue(dbData, new TypeReference<>() {});
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Failed to convert JSON to list", e);
+    }
+  }
+}
