763ad0ab58acd762f5bca3b77e3f548f50451b78
PARK Jaeyoung <estel2005@naver.com>
2025-03-25 14:53:36 +0900

    feat: 물품 상세 조회 위한 임시 LikeStatus 단건 조회 로직 추가
    Former-commit-id: 41ab92dc825c32438dd57734eedcf4413ffe99ec

--- backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/LikeService.java
+++ backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/LikeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class LikeService {
 
+<<<<<<< HEAD
   private final LikeReader likeReader;
   private final LikeProcessor likeProcessor;
 
@@ -35,5 +36,21 @@ public class LikeService {
 
   public void deleteLike(User user, Long goodsId) {
     likeProcessor.deleteLike(user, goodsId);
+  }
+
+  public LikeStatus findLikeStatus(User user, Long goodsId) {
+    /*
+     * 해당하는 goodsId에 대해 아래 정보들을 조회해야 합니다:
+     *
+     * 1. 전체 좋아요 수
+     *    - likeRepository.countByGoodsId(goodsId)
+     *
+     * 2. 해당 사용자가 좋아요를 눌렀는지 여부
+     *    - likeRepository.existsByGoodsIdAndUserId(goodsId, user.id())
+     *
+     * 결과는 goodsId를 기준으로 LikeStatus 객체로 만들어 반환해야 합니다:
+     *   new LikeStatus(goodsId, likedUsersCount, likedByUser)
+     */
+    throw new UnsupportedOperationException("아직 구현되지 않았습니다.");
   }
 }
