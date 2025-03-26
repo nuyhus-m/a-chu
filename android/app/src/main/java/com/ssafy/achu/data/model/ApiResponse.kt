22e25e08713dd9db3297c6a96292d08fbcb7b540
nuyhus-m <minsh2344@gmail.com>
2025-03-26 15:33:33 +0900

    Feat[AOS] - ApiResponse에 error 추가

--- android/app/src/main/java/com/ssafy/achu/data/model/ApiResponse.kt
+++ android/app/src/main/java/com/ssafy/achu/data/model/ApiResponse.kt
@@ -2,5 +2,11 @@ package com.ssafy.achu.data.model
 
 data class ApiResponse<T>(
     val result: String,
-    val data: T
+    val data: T,
+    val error: Error
 )
+
+data class Error(
+    val code: String,
+    val message: String
+)                                     
