a3cbb0e6a88b2ac3bf4d63e732e724bfd98fea43
김덕윤 <duk9741@gmail.com>
2025-03-26 16:59:49 +0900

    feat: 비밀번호 변경 서비스 레이어 작성
    Former-commit-id: 3ac0c03a7e201b5e5651368c1b82ce472c80034e

--- backend/auth-user/src/main/java/com/ssafy/s12p21d206/achu/auth/domain/user/AuthUserModifier.java
+++ backend/auth-user/src/main/java/com/ssafy/s12p21d206/achu/auth/domain/user/AuthUserModifier.java
@@ -1,21 +1,39 @@
 package com.ssafy.s12p21d206.achu.auth.domain.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthUserModifier {
 
-  private final AuthUserRepository authUserRepository;
+  private final AuthUserReader authUserReader;
   private final AuthUserValidator authUserValidator;
 
+  private final AuthUserRepository authUserRepository;
+
+  private final PasswordEncoder passwordEncoder;
+
   public AuthUserModifier(
-      AuthUserRepository authUserRepository, AuthUserValidator authUserValidator) {
-    this.authUserRepository = authUserRepository;
+      AuthUserReader authUserReader,
+      AuthUserValidator authUserValidator,
+      AuthUserRepository authUserRepository,
+      PasswordEncoder passwordEncoder) {
+    this.authUserReader = authUserReader;
     this.authUserValidator = authUserValidator;
+    this.authUserRepository = authUserRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public AuthUser modifyNickname(Long userId, String nickname) {
     authUserValidator.validateNicknameUnique(nickname);
     return authUserRepository.modifyNickname(userId, nickname);
+  }
+
+  public AuthUser modifyPassword(Long userId, String oldPassword, String newPassword) {
+    AuthUser authUser = authUserReader.findAuthUser(userId);
+    authUser.validatePassword(passwordEncoder, oldPassword);
+
+    String encodedPassword = passwordEncoder.encode(oldPassword);
+    return authUserRepository.modifyPassword(userId, encodedPassword);
   }
 }
