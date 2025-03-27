5c3102604bdaee7a726644736b30a89e7e4574b8
nuyhus-m <minsh2344@gmail.com>
2025-03-27 17:48:02 +0900

    Feat[AOS] - Token 정보 관리하는 SharedPreferences 구현

--- android/app/src/main/java/com/ssafy/achu/data/database/SharedPreferencesUtil.kt
+++ android/app/src/main/java/com/ssafy/achu/data/database/SharedPreferencesUtil.kt
@@ -2,15 +2,30 @@ package com.ssafy.achu.data.database
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.google.gson.Gson
+import com.ssafy.achu.data.model.auth.TokenResponse
 
 class SharedPreferencesUtil(context: Context) {
 
     companion object {
         private const val SHARED_PREFERENCES_NAME = "achu_preference"
-        private const val COOKIES_KEY_NAME = "cookies"
     }
 
     private var preferences: SharedPreferences =
         context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
 
+    fun saveTokens(tokenResponse: TokenResponse) {
+        preferences.edit {
+            val gson = Gson()
+            val json = gson.toJson(tokenResponse)  // 객체를 JSON 문자열로 변환
+            putString("token_data", json)
+        }
+    }
+
+    fun getTokens(context: Context): TokenResponse? {
+        val gson = Gson()
+        val json = preferences.getString("token_data", null)
+        return json?.let { gson.fromJson(it, TokenResponse::class.java) }  // JSON 문자열을 객체로 변환
+    }
 }
