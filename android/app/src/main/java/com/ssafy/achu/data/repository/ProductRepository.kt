59876107e09ee0b793f3c0ab5585bf5e023d34e9
민수현 <minsh2344@gmail.com>
2025-03-29 20:08:29 +0900

    Feat[AOS] - 카테고리, 물품목록조회 API 연동(S12P21D206-162)

--- android/app/src/main/java/com/ssafy/achu/data/repository/ProductRepository.kt
+++ android/app/src/main/java/com/ssafy/achu/data/repository/ProductRepository.kt
@@ -1,4 +1,47 @@
 package com.ssafy.achu.data.repository
 
+import com.ssafy.achu.data.model.ApiResult
+import com.ssafy.achu.data.model.product.CategoryResponse
+import com.ssafy.achu.data.model.product.ProductResponse
+import com.ssafy.achu.data.network.RetrofitUtil
+
 class ProductRepository {
+
+    private val productService = RetrofitUtil.productService
+
+    suspend fun getCategoryList(): Result<ApiResult<List<CategoryResponse>>> {
+        return runCatching {
+            productService.getCategoryList()
+        }
+    }
+
+    suspend fun getProductListByCategory(
+        categoryId: Int,
+        offset: Int,
+        limit: Int,
+        sort: String
+    ): Result<ApiResult<List<ProductResponse>>> {
+        return runCatching {
+            productService.getProductListByCategory(
+                categoryId = categoryId,
+                offset = offset,
+                limit = limit,
+                sort = sort
+            )
+        }
+    }
+
+    suspend fun getProductList(
+        offset: Int,
+        limit: Int,
+        sort: String
+    ): Result<ApiResult<List<ProductResponse>>> {
+        return runCatching {
+            productService.getProductList(
+                offset = offset,
+                limit = limit,
+                sort = sort
+            )
+        }
+    }
 }
