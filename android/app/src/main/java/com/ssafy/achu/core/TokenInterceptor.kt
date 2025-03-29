89d32af88de315c756b167ea19ec6a7fbc04b37e
민수현 <minsh2344@gmail.com>
2025-03-30 01:39:36 +0900

    Fix[AOS] - 사용자 인증 버그 수정

--- android/app/src/main/java/com/ssafy/achu/core/TokenInterceptor.kt
+++ android/app/src/main/java/com/ssafy/achu/core/TokenInterceptor.kt
@@ -1,21 +1,31 @@
 package com.ssafy.achu.core
 
+import android.util.Log
+import com.ssafy.achu.core.ApplicationClass.Companion.sharedPreferencesUtil
 import okhttp3.Interceptor
 
 private const val TAG = "TokenInterceptor"
 
-class TokenInterceptor(private val accessToken: String) : Interceptor {
+class TokenInterceptor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+
+        val token = sharedPreferencesUtil.getTokens() ?: return chain.proceed(chain.request())
         val originalRequest = chain.request()
+        val isNeedAccessToken = shouldAddToken(originalRequest)
 
         // 헤더를 추가할지 결정하는 로직
-        val newRequest = if (shouldAddToken(originalRequest)) {
+        val newRequest = if (isNeedAccessToken) {
             originalRequest.newBuilder()
-                .addHeader("Authorization", "Bearer $accessToken")
+                .addHeader("Authorization", "${token.tokenType} ${token.accessToken}")
                 .build()
         } else {
             originalRequest
         }
+
+        Log.d(
+            TAG,
+            "intercept addHeader:${isNeedAccessToken} url:${originalRequest.url}"
+        )
 
         return chain.proceed(newRequest)
     }
