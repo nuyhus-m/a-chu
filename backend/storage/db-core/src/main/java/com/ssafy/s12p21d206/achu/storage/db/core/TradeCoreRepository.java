d9780e03417556e24f1f1ea243320e28f3b4d098
PARK Jaeyoung <estel2005@naver.com>
2025-03-31 14:20:39 +0900

    fix: tradeType enum → String 변경
    Former-commit-id: 3f838999d5c2192fbb7a9eed947567ea65cd40ad

--- backend/storage/db-core/src/main/java/com/ssafy/s12p21d206/achu/storage/db/core/TradeCoreRepository.java
+++ backend/storage/db-core/src/main/java/com/ssafy/s12p21d206/achu/storage/db/core/TradeCoreRepository.java
@@ -4,10 +4,10 @@ import com.ssafy.s12p21d206.achu.domain.*;
 import com.ssafy.s12p21d206.achu.domain.error.CoreErrorType;
 import com.ssafy.s12p21d206.achu.domain.error.CoreException;
 import com.ssafy.s12p21d206.achu.domain.support.SortType;
+import com.ssafy.s12p21d206.achu.storage.db.core.support.SortUtils;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -36,16 +36,10 @@ public class TradeCoreRepository implements TradeRepository {
   @Override
   public List<Trade> findTradedGoods(
       User user, TradeType tradeType, Long offset, Long limit, SortType sort) {
-    Pageable pageable = PageRequest.of(offset.intValue(), limit.intValue(), convertSort(sort));
+    Pageable pageable =
+        PageRequest.of(offset.intValue(), limit.intValue(), SortUtils.convertSort(sort));
     List<TradeEntity> tradeEntities = tradeJpaRepository.findByUserIdAndEntityStatus(
-        user.id(), tradeType, pageable, EntityStatus.ACTIVE);
+        user.id(), tradeType.name(), pageable, EntityStatus.ACTIVE);
     return tradeEntities.stream().map(TradeEntity::toTrade).toList();
-  }
-
-  private Sort convertSort(SortType sort) {
-    return switch (sort) {
-      case LATEST -> Sort.by(Sort.Direction.DESC, "createdAt");
-      case OLDEST -> Sort.by(Sort.Direction.ASC, "createdAt");
-    };
   }
 }
