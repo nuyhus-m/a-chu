98ff8d20036de70bb192a05c79d503c6dbc8593b
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-04-02 01:37:20 +0900

    Feat[AOS] - 마이페이지 찜목록 기능구현

--- android/app/src/main/java/com/ssafy/achu/core/components/LargeLikeItem.kt
+++ android/app/src/main/java/com/ssafy/achu/core/components/LargeLikeItem.kt
@@ -1,8 +1,8 @@
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,42 +14,44 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.ssafy.achu.R
 import com.ssafy.achu.core.theme.AchuTheme
 import com.ssafy.achu.core.theme.FontGray
 import com.ssafy.achu.core.theme.FontPink
-import com.ssafy.achu.core.theme.LightGray
 import com.ssafy.achu.core.theme.White
 
 @Composable
 fun LargeLikeItem(
-    isLiked: Boolean,
     state: String,
     onClickItem: () -> Unit,
-    onClickHeart: () -> Unit,
+    productLike: () -> Unit,
+    productUnlike: () -> Unit,
     productName: String,
     price: String,
-    img: Painter? = null
+    img: Uri
 ) {
+
+    var isLiked by remember { mutableStateOf(true) } // 하트 상태 관리
+
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp // 전체 화면 너비
     val itemWidth = screenWidth * 0.42f
 
@@ -80,19 +82,28 @@ fun LargeLikeItem(
                         .aspectRatio(1f)
                         .clip(RoundedCornerShape(8.dp))
                 ) {
-                    img?.let {
-                        Image(
-                            painter = img,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1f) // 정사각형
-                                .clip(RoundedCornerShape(8.dp)),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
+                    Image(
+                        painter = painterResource(R.drawable.img_miffy_doll), //기본이미지 설정
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1f) // 정사각형
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
 
-                    if (state == "거래완료") {
+                    AsyncImage(
+                        model = img,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1f) // 정사각형
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+
+
+                    if (state == "SOLD") {
                         Box(
                             modifier = Modifier
                                 .fillMaxWidth()
@@ -128,14 +139,12 @@ fun LargeLikeItem(
                     verticalAlignment = Alignment.Bottom
                 ) {
 
-
                     Text(
                         text = price,
                         style = AchuTheme.typography.semiBold16Pink,
                         maxLines = 1, // 한 줄만 표시
                         overflow = TextOverflow.Ellipsis, // 넘치는 부분은 "..."으로 표시
                         modifier = Modifier.weight(0.8f)
-
                     )
 
                     Image(
@@ -144,11 +153,19 @@ fun LargeLikeItem(
                         colorFilter = ColorFilter.tint(if (isLiked) FontPink else FontGray),
                         modifier = Modifier
                             .width(24.dp)
-                            .height(24.dp).weight(0.2f)
+                            .height(24.dp)
+                            .weight(0.2f)
                             .clickable(
                                 interactionSource = remember { MutableInteractionSource() },
                                 indication = null,
-                                onClick = onClickHeart
+                                onClick = {
+                                    isLiked = !isLiked
+                                    if (isLiked) {
+                                        productLike()
+                                    } else {
+                                        productUnlike()
+                                    }
+                                }
                             )
                             .padding(end = 4.dp)
                     )
@@ -159,44 +176,43 @@ fun LargeLikeItem(
     }
 }
 
-@Preview
-@Composable
-fun preItem2() {
-    AchuTheme {
-        Row(modifier = Modifier.padding(24.dp)) {
-            LargeLikeItem(
-                isLiked = true,
-                state = "판매중",
-                onClickItem = {
-                    // 아이템 전체 클릭 시 동작
-                    println("아이템 클릭됨")
-                },
-                onClickHeart = {
-                    println("하트 클릭됨")
-                },
-                productName = "유아식기",
-                price = "500,000,000원",
-                img = ColorPainter(LightGray)
-            )
 
-            Spacer(Modifier.width(16.dp))
-
-            LargeLikeItem(
-                isLiked = false,
-                state = "판매중",
-                onClickItem = {
-                    // 아이템 전체 클릭 시 동작
-                    println("아이템 클릭됨")
-                },
-                onClickHeart = {
-                    println("하트 클릭됨")
-                },
-                productName = "유아식기",
-                price = "5,000",
-                img = ColorPainter(LightGray)
-            )
-
-        }
-
-    }
-}
+//@Preview
+//@Composable
+//fun preItem2() {
+//    AchuTheme {
+//        Row(modifier = Modifier.padding(24.dp)) {
+////            LargeLikeItem(
+////                isLiked = true,
+////                state = "판매중",
+////                onClickItem = {
+////                    // 아이템 전체 클릭 시 동작
+////                    println("아이템 클릭됨")
+////                },
+////                onClickHeart = {
+////                    println("하트 클릭됨")
+////                },
+////                productName = "유아식기",
+////                price = "500,000,000원",
+////            )
+////
+////            Spacer(Modifier.width(16.dp))
+////
+////            LargeLikeItem(
+////                isLiked = false,
+////                state = "판매중",
+////                onClickItem = {
+////                    // 아이템 전체 클릭 시 동작
+////                    println("아이템 클릭됨")
+////                },
+////                onClickHeart = {
+////                    println("하트 클릭됨")
+////                },
+////                productName = "유아식기",
+////                price = "5,000",
+////            )
+////
+////        }
+//
+////    }
+//}
