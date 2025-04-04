11a7dadfd5a28fdedc9ddfe735f517263305a471
김덕윤 <duk9741@gmail.com>
2025-04-04 23:30:44 +0900

    feat: 채팅 모델 구조 개선 및 새 응답 모델 추가
    Former-commit-id: 28b3bf0c65dca491da03382396af52e70f22e511

--- backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/ChatRoom.java
+++ backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/ChatRoom.java
@@ -1,20 +1,33 @@
 package com.ssafy.s12p21d206.achu.chat.domain;
 
+import com.ssafy.s12p21d206.achu.chat.domain.goods.Goods;
 import com.ssafy.s12p21d206.achu.chat.domain.user.ChatUser;
+import com.ssafy.s12p21d206.achu.chat.domain.user.ChatUserProfile;
 import java.util.Objects;
 
 public record ChatRoom(
-    Long id, Long goodsId, ChatUser seller, ChatUser buyer, ParticipantStatus participantStatus) {
+    Long id,
+    Goods goods,
+    ChatUserProfile seller,
+    ChatUserProfile buyer,
+    ParticipantStatus participantStatus,
+    Message lastMessage) {
+
+  public ChatRoom updateLastMessage(Message message) {
+    return new ChatRoom(id, goods, seller, buyer, participantStatus, message);
+  }
 
   public ChatUser findPartner(ChatUser user) {
-    if (!Objects.equals(seller, user) || !(Objects.equals(buyer, user))) {
+    if (!Objects.equals(seller.id(), user.id()) || !(Objects.equals(buyer.id(), user.id()))) {
       throw new IllegalArgumentException("채팅 참여자가 아닙니다.");
     }
 
-    return Objects.equals(seller, user) ? buyer : seller;
+    return Objects.equals(seller.id(), user.id())
+        ? new ChatUser(buyer.id())
+        : new ChatUser(seller.id());
   }
 
   public boolean isParticipant(ChatUser user) {
-    return this.seller.equals(user) || this.buyer.equals(user);
+    return this.seller.id().equals(user.id()) || this.buyer.id().equals(user.id());
   }
 }
--- backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/MessageEventNotifier.java
+++ backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/MessageEventNotifier.java
@@ -7,27 +7,50 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageEventNotifier {
 
-  private static final String MESSAGE_NOTIFICATION_DESTINATION_FORMAT =
+  private static final String NEW_MESSAGE_ARRIVED_DESTINATION_FORMAT =
       "/read/chat/users/%s/message-arrived";
+  private static final String CHAT_ROOM_UPDATE_DESTINATION_FORMAT =
+      "/read/chat/users/%s/rooms/update";
+
   private static final String NEW_MESSAGE_NOTIFICATION_PAYLOAD = "NEW_MESSAGE_ARRIVED";
 
   private final SimpMessagingTemplate messagingTemplate;
+  private final ChatRoomReader chatRoomReader;
 
-  public MessageEventNotifier(SimpMessagingTemplate messagingTemplate) {
+  public MessageEventNotifier(
+      SimpMessagingTemplate messagingTemplate, ChatRoomReader chatRoomReader) {
     this.messagingTemplate = messagingTemplate;
+    this.chatRoomReader = chatRoomReader;
   }
 
   /**
-   * 메시지 도착 이벤트를 수신자에게 전송합니다.
+   * 메시지 도착 이벤트를 전송합니다.
    */
-  public void notifyMessageArrivedToPartner(ChatRoom chatRoom, Message message) {
+  public void notifyMessageArrived(ChatUser user, Message message) {
     if (message.isUserMessage()) {
       return;
     }
 
-    ChatUser partner = chatRoom.findPartner(message.sender());
     messagingTemplate.convertAndSend(
-        String.format(MESSAGE_NOTIFICATION_DESTINATION_FORMAT, partner.id()),
+        String.format(NEW_MESSAGE_ARRIVED_DESTINATION_FORMAT, user.id()),
         NEW_MESSAGE_NOTIFICATION_PAYLOAD);
+  }
+
+  public void notifyChatRoomUpdate(
+      ChatUser user, ChatUser partner, ChatRoom chatRoom, Message message) {
+    if (!message.isUserMessage()) {
+      return;
+    }
+
+    chatRoom = chatRoom.updateLastMessage(message);
+    UnreadCount partnerUnreadCount = chatRoomReader.countUnreadMessages(partner, chatRoom.id());
+    UnreadCount userUnreadCount = chatRoomReader.countUnreadMessages(user, chatRoom.id());
+
+    messagingTemplate.convertAndSend(
+        String.format(CHAT_ROOM_UPDATE_DESTINATION_FORMAT, user.id()),
+        new ChatRoomWithUnreadCount(chatRoom, userUnreadCount));
+    messagingTemplate.convertAndSend(
+        String.format(CHAT_ROOM_UPDATE_DESTINATION_FORMAT, partner.id()),
+        new ChatRoomWithUnreadCount(chatRoom, partnerUnreadCount));
   }
 }
