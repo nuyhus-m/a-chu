9acd3c3b4b5454799bc34cf5f5471b62908c71fd
김덕윤 <duk9741@gmail.com>
2025-04-05 05:29:09 +0900

    refactor:스트림 collect를 toList로 변경
    Former-commit-id: 48659047e08d14b7b9ef7acad0758574c560ff76

--- backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/message/MessageDto.java
+++ backend/chat/src/main/java/com/ssafy/s12p21d206/achu/chat/domain/message/MessageDto.java
@@ -5,7 +5,6 @@ import com.ssafy.s12p21d206.achu.chat.domain.MessageType;
 import com.ssafy.s12p21d206.achu.chat.domain.user.ChatUser;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public record MessageDto(
     Long id,
@@ -27,8 +26,6 @@ public record MessageDto(
   }
 
   public static List<MessageDto> listFrom(List<Message> messages, ChatUser currentUser) {
-    return messages.stream()
-        .map(message -> from(message, currentUser))
-        .collect(Collectors.toList());
+    return messages.stream().map(message -> from(message, currentUser)).toList();
   }
 }
