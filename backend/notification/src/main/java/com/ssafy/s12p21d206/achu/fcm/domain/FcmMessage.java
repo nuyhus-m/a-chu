7fca346af0f5689836e67f02a2eef6fdf5a2a41d
PARK Jaeyoung <estel2005@naver.com>
2025-04-10 02:30:26 +0900

    fix: fcmMessage record 처음으로 변경
    Former-commit-id: 73ee614d87aa900edae14e10b64a9a6bf458b432

--- backend/notification/src/main/java/com/ssafy/s12p21d206/achu/fcm/domain/FcmMessage.java
+++ backend/notification/src/main/java/com/ssafy/s12p21d206/achu/fcm/domain/FcmMessage.java
@@ -2,4 +2,4 @@ package com.ssafy.s12p21d206.achu.fcm.domain;
 
 import java.util.Map;
 
-public record FcmMessage(String to, Map<String, String> data) {}
+public record FcmMessage(String token, String title, String body, Map<String, String> data) {}
