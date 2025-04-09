9724ff5c94bc75422f2084089c40936049ee0e60
PARK Jaeyoung <estel2005@naver.com>
2025-04-09 19:59:01 +0900

    fix: 물품, 추억 관련 정규식에서 이모지 입력 안되게 수정
    Former-commit-id: af122ac95198b49b8684c5c86cfe7ef03495c981

--- backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/validation/GoodsDescriptionValidator.java
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/validation/GoodsDescriptionValidator.java
@@ -3,7 +3,7 @@ package com.ssafy.s12p21d206.achu.api.validation;
 public class GoodsDescriptionValidator extends RegexValidator<GoodsDescription> {
 
   private static final String REGEX =
-      "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣\\p{P}\\p{So}\\p{Zs}+=`~\\\\\\$\\^<>\\\"']{2,200}$";
+      "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣\\p{P}\\p{Zs}+=`~\\\\\\$\\^<>\\\"']{2,200}$";
 
   @Override
   public boolean isValid(String value, jakarta.validation.ConstraintValidatorContext context) {
--- backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/validation/MemoryContent.java
+++ backend/core/core-api/src/main/java/com/ssafy/s12p21d206/achu/api/validation/MemoryContent.java
@@ -4,8 +4,7 @@ import jakarta.validation.Payload;
 
 public @interface MemoryContent {
 
-  String message() default
-      "추억 등록 시 내용은 한글, 영어, 숫자, 특수문자, 이모지, 공백 포함 2~100자여야 하며, 앞뒤 공백은 허용되지 않습니다.";
+  String message() default "추억 등록 시 내용은 한글, 영어, 숫자, 특수문자 포함 2~100자여야 하며, 앞뒤 공백과 이모지는 허용되지 않습니다.";
 
   Class<?>[] groups() default {};
 
