e8571b6a8b41d48807c5a42918f7f84bc0979aeb
민수현 <minsh2344@gmail.com>
2025-04-09 19:51:19 +0900

    Fix[AOS] - 키보드 버그 수정

--- android/app/src/main/java/com/ssafy/achu/ui/chat/chatdetail/ChatScreen.kt
+++ android/app/src/main/java/com/ssafy/achu/ui/chat/chatdetail/ChatScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -131,6 +132,7 @@ fun ChatScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(color = White)
+            .imePadding()
     ) {
         CustomChatTopBar(
             partner = uiState.partner ?: Partner(
