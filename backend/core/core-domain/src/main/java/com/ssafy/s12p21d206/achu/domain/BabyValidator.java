eccb92009e9da1c26c32a0ff946d6aeee9fb7f54
김덕윤 <duk9741@gmail.com>
2025-04-09 23:54:36 +0900

    feat: 물건 조회 babyId 0으로 요청시에도 동작하도록 변경
    Former-commit-id: 127fcf2c4cd626a841af82dd77ee66bcc4d27207

--- backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/BabyValidator.java
+++ backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/BabyValidator.java
@@ -19,6 +19,12 @@ public class BabyValidator {
   }
 
   public void validateParent(User user, Long id) {
+    // client에서 가격변동 알림에서 접속 시 babyId를 0으로 보내기로함
+    // TODO: 추후에 가격변동 알림에서 아기 정보도 함께 보내는 것으로 변경해야함
+    if (id == 0) {
+      return;
+    }
+
     if (!babyRepository.existsByIdAndUser(id, user)) {
       throw new CoreException(CoreErrorType.CANNOT_ACCESS_BABY);
     }
--- backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/PreferenceScoreAdder.java
+++ backend/core/core-domain/src/main/java/com/ssafy/s12p21d206/achu/domain/PreferenceScoreAdder.java
@@ -11,6 +11,11 @@ public class PreferenceScoreAdder {
   }
 
   public void addScore(Long babyId, Long goodsId, Long score) {
+    // client 에서 가격변동 알림에서 접속 시 babyId를 0으로 보내기로함
+    // TODO: 추후에 가격변동 알림에서 아기 정보도 함께 보내는 것으로 변경해야함
+    if (babyId == 0) {
+      return;
+    }
     preferenceScoreRepository.addScore(babyId, goodsId, score);
   }
 }
